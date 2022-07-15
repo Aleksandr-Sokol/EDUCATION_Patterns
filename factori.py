c28605b10deac64d2432652131cbc17ee889afc2
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2022-07-15 15:16:37 +0500

    Add factory

--- factori.py
+++ factori.py
@@ -1,1 +1,26 @@
 # Pattern (шаблон) фабрика
+# Порождающие метод
+# Фабричный метод - Общий интерфейс для создания разных объектов, позволяющий подклассам определять тип создаваемого объекта.
+
+# Используется:
+# библиотека factory-boy, для генерации фабрик экземпляров для тестирования
+# https://egorovegor.ru/testing-django-app/
+
+from abc import ABC, abstractmethod
+
+class Worker:
+    pass
+class Unemployed:
+    pass
+
+class PersonFactory(ABC):
+    """
+    Worker – рабочий
+    Unemployed – безработный
+    они указываются в type_of_person
+    """
+    def get_person(self, type_of_person):
+        if type_of_person == "worker":
+            return Worker()
+        if type_of_person == "unemployed":
+            return Unemployed()
--- start.py
+++ start.py
@@ -1,8 +1,14 @@
 from singleton import Singleton  # синглтон
+from factori import PersonFactory # фабрика
 
-# синглтон
+print('синглтон')
 ex1 = Singleton()
 ex2 = Singleton()
 ex2.a = 123
 print(ex1.a)
+
+print('фабрика')
+person1 = PersonFactory().get_person('worker')
+person2 = PersonFactory().get_person('unemployed')
+print(f'types. person1 {type(person1)}, person2: {type(person2)}')
 
