8676f1bd56c41d7761716a5d21bf4d2b2b66d88b
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2022-07-15 15:46:07 +0500

    Add decorator

--- decotator.py
+++ decotator.py
@@ -1,4 +1,31 @@
 # Pattern (шаблон) Декоратор
-# Структурные шаблон добавляет, убирает или изменяет поведение декорированного объекта.
+#  убирает или изменяет поведение декорированного объекта.
+
+# тип: Структурный
 
 # Используется:
+# для добавления новых функций, без изменения объекта
+# 1. Для распределенных задач в cellery
+# 2. для создания фикстур в тестировании
+
+# Декоратор без параметра
+import time
+
+def time_work(func):
+    def wrapper(*args, **kwargs):
+        t0 = time.process_time()
+        f = func(*args, **kwargs)
+        print(f"time work: {time.process_time()-t0}")
+        return f
+    return wrapper
+
+# Декоратор с параметром
+def time_text(some_text: str = ''):
+    def time_work(func):
+        def wrapper(*args, **kwargs):
+            t0 = time.process_time()
+            f = func(*args, **kwargs)
+            print(f"{some_text}: {time.process_time() - t0}")
+            return f
+        return wrapper
+    return time_work
--- start.py
+++ start.py
@@ -1,14 +1,22 @@
 from singleton import Singleton  # синглтон
 from factori import PersonFactory # фабрика
+from decotator import time_text # Декоратор
 
-print('синглтон')
+print('синглтон:')
 ex1 = Singleton()
 ex2 = Singleton()
 ex2.a = 123
 print(ex1.a)
 
-print('фабрика')
+print('фабрика:')
 person1 = PersonFactory().get_person('worker')
 person2 = PersonFactory().get_person('unemployed')
 print(f'types. person1 {type(person1)}, person2: {type(person2)}')
+
+print('Декоратор:')
+@time_text('working fun: ')
+def fun():
+    return 1
+print(fun())
+
 
